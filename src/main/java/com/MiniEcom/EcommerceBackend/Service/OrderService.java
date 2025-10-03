9861c47b503ae91ef9bd1c3f852f766a19d8801a
Arnav242005 <jadhavarnav58@gmail.com>
2025-10-03 11:28:16 +0530

    updated logic for OrderService

--- src/main/java/com/MiniEcom/EcommerceBackend/Service/OrderService.java
+++ src/main/java/com/MiniEcom/EcommerceBackend/Service/OrderService.java
@@ -1,12 +1,15 @@
 package com.MiniEcom.EcommerceBackend.Service;
 
 import com.MiniEcom.EcommerceBackend.Repository.OrderRepo;
+import com.MiniEcom.EcommerceBackend.model.Order;
 import com.MiniEcom.EcommerceBackend.model.dto.OrderRequest;
 import com.MiniEcom.EcommerceBackend.model.dto.OrderResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class OrderService {
@@ -15,7 +18,14 @@ public class OrderService {
     private OrderRepo repo;
 
     public OrderResponse placeOrder(OrderRequest orderRequest) {
-
+        Order order = new Order();
+        String orderID = UUID.randomUUID().toString().substring(0,8).toUpperCase();
+        order.setOrderId(orderID);
+        order.setCustomerName(orderRequest.customerName());
+        order.setEmail(orderRequest.email());
+        order.setOrderDate(LocalDate.now());
+        order.setStatus("PLACED");
+        return null;
     }
 
     public List<OrderResponse> getAllorderResponses() {
